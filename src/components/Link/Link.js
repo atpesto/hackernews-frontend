f726fa2c2791f0c36a39f007c0be928251b2be86
Nikhil Arora <narora200@gmail.com>
2018-08-11 11:36:54 +0530

    [Feat]: Votes Mutation and Store(cache) update

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -1,13 +1,78 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { gql } from 'apollo-boost';
+import { Mutation } from 'react-apollo';
+
+import { timeDifferenceForDate } from '../../utils/timeDifference';
+import { AUTH_TOKEN } from '../../config/constants';
+
+
+const VOTE_MUTATION = gql`
+  mutation VoteMutation($linkId: ID!) {
+    vote(linkId: $linkId) {
+      id
+      link {
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`;
 
 class Link extends Component {
   render() {
-    const { link: { description, url } } = this.props;
+    const { link, index, updateStoreAfterVote } = this.props;
+    const authToken = localStorage.getItem(AUTH_TOKEN);
     return (
-      <div>
-        <div>
-          {description} ({url})
+      <div className="flex mt2 items-start">
+        <div className="flex items-center">
+          <span className="gray">{index + 1}.</span>
+          {
+            authToken
+            && (
+              <Mutation
+                mutation={VOTE_MUTATION}
+                variables={{ linkId: link.id }}
+                update={(store, { data: { vote } }) =>
+                  updateStoreAfterVote(store, vote, link.id)
+                }
+              >
+                {
+                  voteMutation => (
+                    <div
+                      role="button"
+                      tabIndex={0}
+                      className="ml1 gray f11"
+                      onClick={voteMutation}
+                    >
+                      ▲
+                    </div>
+                  )
+                }
+              </Mutation>
+            )
+          }
+        </div>
+        <div className="ml1">
+          <div>
+            {link.description} ({link.url})
+          </div>
+          <div className="f6 lh-copy gray">
+            {link.votes.length} votes | by{' '}
+            {link.postedBy
+              ? link.postedBy.name
+              : 'Unknown'}{' '}
+            {
+              timeDifferenceForDate(link.createdAt)
+            }
+          </div>
         </div>
       </div>
     );
@@ -20,6 +85,8 @@ Link.propTypes = {
     description: PropTypes.string,
     url: PropTypes.string,
   }).isRequired,
+  index: PropTypes.number.isRequired,
+  updateStoreAfterVote: PropTypes.func.isRequired,
 };
 
 export default Link;
